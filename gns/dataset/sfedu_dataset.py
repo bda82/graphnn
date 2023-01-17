ff89c04fdabd1f7ce20626eb555268acd115ba99
Paul Rock <paul@drteam.rocks>
2023-01-17 23:37:02 +0300

    A lot of tunes in sfedu_dataset

--- gns/dataset/sfedu_dataset.py
+++ gns/dataset/sfedu_dataset.py
@@ -4,7 +4,6 @@ import logging
 import numpy as np
 from gns.graph.graph import Graph
 from gns.dataset.dataset import Dataset
-from gns.transformation.normalized_adjacency import NormalizeAdjacencyMatrix
 
 logger = logging.getLogger(__name__)
 
@@ -13,33 +12,29 @@ class SfeduDataset(Dataset):
     """
     Example for competence Dataset.
     """
+
     def __init__(
-        self,
-        n_samples=100,
-        n_count=20,
-        n_features=3,
-        e_features=3,
-        p=0.1,
-        **kwargs
+            self,
+            n_samples=100,
+            n_count=20,
+            n_features=3,
+            e_features=3,
+            p=0.1,
+            **kwargs
     ):
         # Amount of graphs
-
         self.n_samples = n_samples
 
         # Count of nodes in each graph
-
         self.n_count = n_count
 
         # Amount of features in each node
-
         self.n_features = n_features
 
         # Amount of features in each edge
-
         self.e_features = e_features
 
         # Probability of creating connection between nodes
-
         self.p = p
 
         super().__init__(**kwargs)
@@ -49,49 +44,40 @@ class SfeduDataset(Dataset):
     #     return os.path.dirname(__file__) + '/' + self.__class__.__name__
 
     def make_graph(self):
-        # Initial parameters
-
         logger.info("Initial parameters...")
-
         n = self.n_count
         node_features = np.random.randint(0, self.n_features, size=n)
 
-        # Build and fill matrix of node features
-
+        # Build X array (nodes features)
         logger.info("Build and fill matrix of node features...")
-
         x = np.zeros((n, self.n_features))
         x[np.arange(n), node_features] = 1
 
-        # Build and fill matrix of edges, then convert to Coordinates array
-
-        logger.info("Build and fill matrix of edges, then convert to Coordinates array...")
-
+        # Build A array (edges between nodes)
+        logger.info("Build and fill matrix of edges...")
         a = np.random.rand(n, n) <= self.p
-        a = np.maximum(a, a).astype(int)
+        a = np.maximum(a, a.T).astype(int)
+        # a = sp.csr_matrix(a)
 
-        # Labels
-
-        logger.info("Build labels...")
-
+        # Build Y array (labels of nodes)
+        logger.info("Build and fill matrix of labels...")
         y = np.zeros((self.n_features,))
         n_features_counts = x.sum(0)
         y[np.argmax(n_features_counts)] = 1
 
         # Return multidimensional array of settings
-
-        return [x, a, y]
+        return [x, a,y]
 
     def download(self):
         # Create the directory
-
         os.makedirs(self.path, exist_ok=True)
 
         # Generate required amount of samples, then save to disk
-
+        logger.info(f"Generate {self.n_samples} graphs...")
+        zeros = len(str(self.n_samples))
         for s in range(self.n_samples):
             graph = self.make_graph()
-            filename = os.path.join(self.path, f'graph_{str(s).zfill(3)}')
+            filename = os.path.join(self.path, f'graph_{str(s).zfill(zeros)}')
             np.savez(
                 filename,
                 x=graph[0],
@@ -101,35 +87,28 @@ class SfeduDataset(Dataset):
 
     def read(self):
         # Read files from disk
+        graphs = glob.glob(self.path + "/graph_*.npz")
 
-        _graphs = glob.glob(self.path + "/graph_*.npz")
-
-        # Output array
-
-        graphs = []
+        # We must return a list of Graph objects
+        output = []
 
         # Read graphs
-
-        for graph in _graphs:
+        for graph in graphs:
             # Read from file
             data = np.load(graph, allow_pickle=True)
 
-            # Process parameters
-
-            x = data['x']
-            a = data['a']
-
-            #a = sp.csr_matrix(a)
-
-            y = data['y']
-
             # Append arrays to Graph object
-
-            graphs.append(Graph(x=x, a=a, y=y))
+            output.append(
+                Graph(
+                    x=data['x'],
+                    a=data['a'],
+                    y=data['y']
+                )
+            )
 
         # Return array of graphs
-        return graphs
+        return output
 
 
-def sfedu_dataset_fabric(n_samples=100, **kwargs):
-    return SfeduDataset(n_samples=n_samples, transforms=NormalizeAdjacencyMatrix(), **kwargs)
+def sfedu_dataset_fabric(n_samples=10, **kwargs):
+    return SfeduDataset(n_samples=n_samples, **kwargs)
